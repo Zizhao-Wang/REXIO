e49fccd65f0fb3b4f1367844f175dda7b8d92d29
ZizhaoWa <81192wzz@gmail.com>
2022-08-28 08:19:28 +0000

    Bug fixed!

--- code/Linearhashing/L-Hashing.h
+++ code/Linearhashing/L-Hashing.h
@@ -7,6 +7,9 @@
 #include <list>
 #include <algorithm>
 
+/*
+ * Hash function. 
+ */
 class Hash
 {
 public:
@@ -16,6 +19,76 @@ public:
 };
 
 
+
+class HashTable:public Hash 
+{
+
+    int max_col;
+    vector<string> _vec;
+
+public:
+
+   /* 
+    * Here are some useful functions for updating hash table:
+    * 1. Inserting a new key/value data pair.
+    * 2. Deleting a Key/Value item.
+    * 3. Searching one or more specific value entries. 
+    */
+
+    int insertion(uint64_t value)
+    {
+        
+    }
+
+    int deletion(uint64_t key)
+    {
+        
+    }
+
+    int Searching(uint64_t key)
+    {
+        
+    }
+
+    HashTable() : _vec(100), max_col(0) {}
+
+    void add(std::string &val) 
+    {
+        int tmp = 0;
+        int res = val[0] - 97;
+        if (_vec[res].empty()) {
+            _vec[res] = val;
+        } else {
+            while (!_vec[res].empty()) {
+                ++tmp;
+                ++res;
+            }
+            _vec[res] = val;
+        }
+        if (tmp > max_col)
+            max_col = tmp;
+    }
+
+    bool check(std::string &val) 
+    {
+        int res = val[0] - 97;
+        if (find(_vec.begin(), _vec.end(), val) != _vec.end())
+            return true;
+        else {
+            while (!_vec[res].empty()) {
+                ++res;
+                if (find(_vec.begin(), _vec.end(), val) != _vec.end())
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    int getMaxCollisions() const 
+    {
+        return max_col;
+    }
+};
 
 /* Declaration of L-Hashing functions */
 void LinearHashing(); // port of L-Hashing

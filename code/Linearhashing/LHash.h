037ce43f58645364558cd35fcc085a3e6a948ae4
ZizhaoWa <81192wzz@gmail.com>
2022-08-29 17:29:41 +0000

    Implement Linear hashing

--- code/Linearhashing/LHash.h
+++ code/Linearhashing/LHash.h
@@ -1,58 +1,51 @@
-/*
- * @author          zz.wang
+/* @author          zz.wang
  * @date            4/8/2022
  * @note            This is header file of "Lhash.cpp".
  * @description     This file contains all fuction declaration and definition of class used in "Lhash.cpp". 
  * @idetification   code/Linearhashing/LHash.h
  */
-
 #ifndef EXPERIMENT1_HEADER_H
 #define EXPERIMENT1_HEADER_H
 
 #include <iostream>
-#include <string>
 #include <vector>
-#include <list>
-#include <algorithm>
+#include "../Backend/IODisk/WriteDisk.h"
 
-
-void LHashPort();
-
-/*
- * Some pre-define variables 
- */
+/*  Some pre-define variables   */
 #define BucketSize 100  // size of a bucket
 #define Tablebase  100  // In-memory table size
 
 
 class LBucket
 {
-  public:
+  private:
     std::vector<uint64_t>  bucket;
-    uint64_t  BucketNo;  
+    uint64_t  BucketNo;
+
+  public:
+    int Insert(uint64_t key1)
+    {
+
+      bucket.insert(key1);
+      return 0;
+
+    }
+
 };
 
 class LinearHashTable
 {
-  /*
-   * Private variables. 
-   */
+  /* Private variables. */
   private:
     std::vector<LBucket> BucketTable;  //In-memeory buckets table with fixed bucket size.
-
     uint64_t Tablesize;
-    uint64_t mod;
-    // vector<set<int>> Overflow;
+    uint64_t mod;    // vector<set<int>> Overflow;
 
   public:
-  /*
-   * some public function to manipulate private variables. 
-   */
-    LinearHashTable(int sz)
+    /* some public function to manipulate private variables. */
+    LinearHashTable()
     {
-      /*
-       * Constructive function is used to initialize private variables. 
-       */
+      /*  Constructive function is used to initialize private variables. */
       Tablesize = Tablebase;
       for(int i = 0; i<Tablesize;i++)
       {
@@ -61,8 +54,8 @@ class LinearHashTable
       }
     }
 
-    // void split(int val)
-    // {
+    int split(int val)
+    {
     //   LBucket.push_back();
     //   for(auto v:Bucket[val])
     //   {
@@ -86,29 +79,34 @@ class LinearHashTable
     // {
     //   for(int i=0;i<mod;i++) Bucket.PB(set<int>()),Overflow.PB(set<int>());
     //   mod*=2;marker=0;
-    // }
+    }
 
-    int insert(int value)
+    int insert(uint64_t key, uint64_t value)
     {
+    /* This function is implemented to insert a special value into a bucket according to
+      * a special rule that usually is called “Linear Hashing method”. 
+      * The following source code contains three steps:
+      *  1. Find a proper bucket
+      *  2. Insert this value into this in-memory bucket 
+      *  4. Synchronize the value with Disk
+      *  3. update in-memory table 
+      */
 
-      /*
-       * This function is implemented to insert a special value into a bucket according to
-       * a special rule that usually is called “Linear Hashing method”. 
-       * The following source code contains three steps:
-       *  1. Find a proper bucket
-       *  2. Insert this value into this in-memory bucket 
-       *  4. Synchronize the value with Disk
-       *  3. update in-memory table 
-       */
-
-      /*
-       * step 1. Find a proper bucket for a specific value 
-       */
-      // int bucketno = value % mod;  
-      // if(Buckets[bucketno].size() >= BucketSize)
-      // {
-      //   bucketno = value % (mod*2);
-      // }
+      /* step 1. Find a proper bucket for a specific value */
+      int bucketno = key % mod;  
+      if(BucketTable[bucketno].size() >= BucketSize)
+      {
+        int err = 0;
+        err = split(bucketno);
+        bucketno = value % (mod*2);
+        BucketTable[bucketno].Insert(key);
+        SingleValueWrite(value,bucketno,BucketTable[bucketno].size());
+      }
+      else
+      {
+        BucketTable[bucketno].Insert(key);
+        SingleValueWrite(value,bucketno,BucketTable[bucketno].size());
+      }
       // if(Buckets[bucketno].count(x)+ Overflow[bucketno].count(x))
       // {
       //   return 0;
@@ -147,5 +145,7 @@ class LinearHashTable
 // void LinearHashing(); // port of L-Hashing
 
 
+/* All function declaration!*/
+void LHashPort();
 
 #endif //CLASSWORK_HEADER_H

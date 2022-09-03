b96c5133a779793c4d3d79d0fa879a64b44f1bbf
ZizhaoWa <81192wzz@gmail.com>
2022-09-03 11:07:55 +0000

    Sync changes

--- code/Linearhashing/LHash.cpp
+++ code/Linearhashing/LHash.cpp
@@ -22,7 +22,7 @@ void LHashPort()
   
     /* Write datum */
     startTime = clock();
-    for(int i=0;i<=1000000;i++)
+    for(int i=0;i<=100000;i++)
     {
       uint64_t value = i;
       hashtable.insert(i,value);

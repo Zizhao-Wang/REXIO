4a0c0efaa3a605dc7e10944e27440126ec83758a
ZizhaoWa <81192wzz@gmail.com>
2022-09-06 16:38:48 +0000

    Sync changes

--- code/LSM-Tree/lsm_tree.h
+++ code/LSM-Tree/lsm_tree.h
@@ -1,21 +1,22 @@
 /**
- * @date 7/9/2022 0:32
+ * @date   0:32, 7/9/2022
  * @author zz.wang
  * @details
  *          If this is the program's first run, the program will create a new special hash table in memory.
  *          Otherwise, the process will read the hash table from the solid-state disk (SSD).
  *
- *         IDENTIFICATION
- *   Experiment1/FrontEnd/HashTable/HashTableCreate.h
+ * IDENTIFICATION:
+ *          code2/code/LSM-Tree/lsm_tree.h
  **/
 
-#include <vector>
+
 
 #include "buffer.h"
 #include "level.h"
 #include "spin_lock.h"
 #include "types.h"
 #include "worker_pool.h"
+#include <vector>
 
 #define DEFAULT_TREE_DEPTH 5
 #define DEFAULT_TREE_FANOUT 10
@@ -23,14 +24,17 @@
 #define DEFAULT_THREAD_COUNT 4
 #define DEFAULT_BF_BITS_PER_ENTRY 0.5
 
+
 class LSMTree 
 {
+private:
     Buffer buffer;
     WorkerPool worker_pool;
     float bf_bits_per_entry;
     vector<Level> levels;
     Run * get_run(int);
     void merge_down(vector<Level>::iterator);
+
 public:
     LSMTree(int, int, int, int, float);
     void put(KEY_t, VAL_t);

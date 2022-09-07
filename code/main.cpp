2c8277a4586c74b55daa01e213ad4de22e357adb
ZizhaoWa <81192wzz@gmail.com>
2022-09-07 04:13:35 +0000

    Sync changes

--- code/main.cpp
+++ code/main.cpp
@@ -9,6 +9,17 @@
 #include "Linearhashing/LHash.h"
 
 
+#define logo "\e[31m\
+ _   _   _____       ___   _____        _____   _       ___   _____  \n\
+| | | | /  ___|     /   | /  ___/      /  ___/ | |     /   | |_   _| \n\
+| | | | | |        / /| | | |___       | |___  | |    / /| |   | |   \n\
+| | | | | |       / / | | \___  \      \___  \ | |   / / | |   | |   \n\
+| |_| | | |___   / /  | |  ___| |       ___| | | |  / /  | |   | |   \n\
+\_____/ \_____| /_/   |_| /_____/      /_____/ |_| /_/   |_|   |_|   \n\e[0m\
+"
+
+
+
 /* Define some global variables. */
 struct nvm_bp* bp = nullptr;
 std::unordered_map<uint64_t,uint64_t> chunkusage;
@@ -36,7 +47,7 @@ int GlobalInitialize(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
-    printf("%s\n",argv[1]);
+    printf(logo);
     /* initialize some global variables and SSD components initialization. */ 
     int result = GlobalInitialize(argc,argv);
     if(result != 0)

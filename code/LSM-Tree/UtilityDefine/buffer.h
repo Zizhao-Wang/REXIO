d011f67902ffbe72897a8a7b94e0d0753e034b61
ZizhaoWa <81192wzz@gmail.com>
2022-09-11 13:10:06 +0000

    Sync changes

--- code/LSM-Tree/UtilityDefine/buffer.h
+++ code/LSM-Tree/UtilityDefine/buffer.h
@@ -15,7 +15,7 @@ public:
     Buffer(int maxsize) : MaxSize(maxsize) {};
     VAL_t * GetValue(KEY_t) const;
     std::vector<entry_t> * GetRange(KEY_t, KEY_t) const;
-    bool put(KEY_t, VAL_t val);
+    bool PutValue(KEY_t, VAL_t val);
     void empty(void);
 
 };

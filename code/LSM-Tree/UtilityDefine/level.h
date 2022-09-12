79a92501af6c2e4d6163b0253eca8b88a8dd8044
ZizhaoWa <81192wzz@gmail.com>
2022-09-12 14:47:34 +0000

    Synchornize changes

--- code/LSM-Tree/UtilityDefine/level.h
+++ code/LSM-Tree/UtilityDefine/level.h
@@ -5,9 +5,12 @@ class Level
 {
 public:
     int LevelNumber;
-    int max_runs;
-    long max_run_size;
-    std::deque<Run> runs;
-    Level(int n, long s) : max_runs(n), max_run_size(s) {}
-    bool remaining(void) const { return max_runs - runs.size();}
+    int MaxRuns;
+    long MaxRunSize;
+    std::deque<Run> Runs;
+    Level(int n, long s) : MaxRuns(n), MaxRunSize(s) {}
+    bool remaining(void) const 
+    { 
+        return max_runs - runs.size();
+    }
 };

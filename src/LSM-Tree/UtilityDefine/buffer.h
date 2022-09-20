5d875eefb4f5c1ea073a2a3b94a12a0652fbee72
ZizhaoWa <81192wzz@gmail.com>
2022-09-20 16:06:57 +0000

    optimize project structure

--- src/LSM-Tree/UtilityDefine/buffer.h
+++ src/LSM-Tree/UtilityDefine/buffer.h
@@ -2,7 +2,12 @@
  * @date:   8/9/2022
  * @author: zz.wang
  * @details:
- *          This file shows how to construct a disk-based LSM-Tree.
+ *          This is a header file for constructing a in-memory buffer(that equals Level 0 in LSM-Tree).
+ *          There are two private variables in "buffer":
+ *          std::set<entry_t> Entries and uint64_t MaxSize. 
+ *          we constructed Entries for storing data passed from memory. 
+ *          With key-values pairs insertig, different size of the set will be recorded. 
+ *          The size must be less than or equal to a threshold called "MaxSzie".
  *
  * IDENTIFICATION:
  *          src/LSM-Tree/UtilityDefine/buffer.h

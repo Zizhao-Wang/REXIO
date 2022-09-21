76b1a484d6dc9b409fba0ea6b4fe08666251ed7d
ZizhaoWa <81192wzz@gmail.com>
2022-09-21 10:45:39 +0000

    Synchornize the structure!

---
+++ src/LazySplit-Hash/LSHash.cpp
@@ -0,0 +1,8 @@
+
+
+
+
+LSHash::LSHash(/* args */)
+{
+}
+
---
+++ src/LazySplit-Hash/LSHash.h
@@ -0,0 +1,30 @@
+/**
+ * @date:   21/9/2022
+ * @author: zz.wang
+ * @email:  zz.wang@siat.ac.cn
+ * @details:
+ *          This file shows how to construct a Lazy-split Hash.
+ *
+ * IDENTIFICATION:
+ *          src/LazySplit-Hash/LSHash.h
+ **/
+
+#ifndef EXPERIMENT1_LAZYSPLITHASH_H
+#define EXPERIMENT1_LAZYSPLITHASH_H
+
+class LSHash
+{
+private:
+	/* data */
+public:
+	LSHash(/* args */);
+	~LSHash();
+};
+
+
+
+
+
+
+
+#endif //EXPERIMENT1_LAZYSPLITHASH_H

3cfc4c7ea9c6102205b4b76607fcbd0428d4dee5
ZizhaoWa <81192wzz@gmail.com>
2022-09-25 13:24:59 +0000

    Sync

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "FrontEnd/MemoryAllocate/HashBlock.h"
 #include "FrontEnd/HashTable/HashTable.h"
 #include "Exhashing/ExtendibleHash.h"
-#include "Linearhashing/LHash.h"
+#include "Multi Linearhashing/LHash.h"
 #include "Auxizilary/Logo.h"
 #include "LSM-Tree/LsmTree.h"
 

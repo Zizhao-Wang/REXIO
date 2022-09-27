85083ef47fcd49795d6a3142e65397a8fe520f9f
ZizhaoWa <81192wzz@gmail.com>
2022-09-27 09:18:29 +0000

    sync code

---
+++ src/Backend/SSDWrite/writer.h
@@ -0,0 +1,13 @@
+/**
+ * @date:   27/9/2022
+ * @author: zz.wang
+ * @details:
+ *          This file is the unique port of this project.
+ * IDENTIFICATION:
+ *          src/Backend/SSDWrite/writer.h
+ *
+ * According to the Google CPP style guide, header file reference should follow the following order:
+ * (To avoid hidden dependencies)
+ * 1. directly related header 2. C library 3. C++ library,
+ * 4. other libraries'.h  5. your project's .h.  
+ **/

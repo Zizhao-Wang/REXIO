b15bae3243ad5799ebd52d6ce95e66fd64847d9c
ZizhaoWa <81192wzz@gmail.com>
2022-09-28 02:41:26 +0000

    sync

---
+++ src/MultiHash/EagerSplitHash/ESHash.h
@@ -0,0 +1,80 @@
+/**
+ * @date:   28/9/2022
+ * @author: zz.wang
+ * @email:  zz.wang@siat.ac.cn
+ * @details:
+ *          This file shows how to construct a Lazy-split Hash.
+ *
+ * IDENTIFICATION:
+ *          src/MultiHash/EagerSplitHash/ESHash.h
+ **/
+
+#ifndef EXPERIMENT1_LAZYSPLITHASH_H
+#define EXPERIMENT1_LAZYSPLITHASH_H
+
+#include <iostream>
+#include <vector>
+#include "../Bucket.h"
+#include "../../Auxizilary/GlobalVariable.h"
+#include "../../Auxizilary/SysOutput.h"
+
+
+#define DEFAULT_BUCKETMAXSIZE 100
+#define DEFAULT_TABLEBASESIZE 100
+#define DEFAULT_IFTHRESHOLD   0.9
+
+class LSbucket : public bucket
+{
+protected:
+	std::vector<SEntry> BucketEntries;
+
+public:
+	LSbucket(uint16_t maxsize);
+
+	int Insert(SKey key, SValue value);
+	SEntry Retrieval(SKey key);
+    int Delete(SKey key);
+    int Update(SKey key, SValue value);
+    
+    bool IsFull(void) const;
+    bool IsEmpty(void) const;
+
+    void AllClear(void);
+	uint16_t GetMaxSize() const;
+	std::vector<SEntry> Getdata() const;
+    int PageWrite();
+
+};
+
+
+class LSHash
+{
+
+private:
+	std::vector<LSbucket> bucketList;
+	uint64_t Allsize;
+	uint16_t bmaxsize;
+	double  IFthreshold;
+
+public:
+	LSHash(uint16_t , uint16_t, double ifth);
+
+	SKey BitHashfunc(SKey, uint8_t);
+	uint8_t  GetBits(SKey);
+	void Split(size_t BucketNo);
+
+	double IFCompute();
+	int Insert(SKey key, SValue value);
+	SEntry Retrieval(SKey key);
+    int Delete(SKey key);
+    int Update(SKey key, SValue value);
+	
+};
+
+
+void LSHashPort();
+
+
+
+
+#endif //EXPERIMENT1_LAZYSPLITHASH_H

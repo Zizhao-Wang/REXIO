78f4624f0fcf66dabd589d69f561c6ea28179986
ZizhaoWa <81192wzz@gmail.com>
2022-10-02 19:12:43 +0000

    sync

---
+++ src/TNCTree/StoreTier/LRU.cpp
@@ -0,0 +1,21 @@
+#include "LRU.h"
+
+std::unordered_map<uint64_t, ReadNode*> ReadBuffer;
+
+int LRUPut(TNCEntry * data)
+{
+
+    return 0;
+
+}
+
+int LRUGet(PageType PageId)
+{
+    return 0;
+
+}
+
+bool IsLRUPosition()
+{
+	return Read
+}

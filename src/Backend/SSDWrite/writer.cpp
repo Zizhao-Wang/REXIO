52ade18061998df2d5168a2a404922b25b31be4f
ZizhaoWa <81192wzz@gmail.com>
2022-10-02 14:04:35 +0000

    sync

--- src/Backend/SSDWrite/writer.cpp
+++ src/Backend/SSDWrite/writer.cpp
@@ -28,101 +28,38 @@ TNCEntry * Pagedata = nullptr;
 /* 
  * Write functions. 
  */
-int WriteintoSector()
-{
-    uint64_t bpn = 0;
-    printf("Starting writing to SSD:\n");
-    if(!bp)
-    {
-        perror("nvm_bp_init failure!");
-        exit(101);
-    }
-
-    int err;
-
-    //Initialize sector address.
-    struct nvm_addr addrs_chunk = nvm_addr_dev2gen(bp->dev,sectorpointer);
-
-    size_t ws_min = nvm_dev_get_ws_min(bp->dev);
-    struct nvm_addr addrs[ws_min];
-
-    for (size_t aidx = 0; aidx < ws_min; ++aidx) 
-    {
-		addrs[aidx].val = addrs_chunk.val;
-		addrs[aidx].l.sectr = writepointer+aidx;
-		//printf("aidx: %lu addrs[aidx].val : %lu chunk_addrs[cidx].val %lu addrs[aidx].l.sectr %lu \n",aidx,addrs[aidx].val,chunk_addrs[cidx].val,addrs[aidx].l.sectr);
-	}
-
-	char * databuffer = (char*) Pagedata;
-    for(int i=0;i<2048*8;i++)
-        bp->bufs->write[i] = databuffer[i];
-    err = nvm_cmd_write(bp->dev, addrs, ws_min,bp->bufs->write, NULL,0x0, NULL);
-    if(!err)
-    {
-        printf("Insert completion! Insert sectors: %ld\n",sectorpointer);
-    }
-
-    bpn = sectorpointer;
-    //update write pointer!
-    sectorpointer+= ws_min;
-    writepointer += ws_min;
-    //update data structure!
-    chunkusage[sectorpointer/4096] = sectorpointer%4096;
-    
-
-             
-    if (writepointer>=4096)
-    {
-        blockpointer++;
-        writepointer = 0;
-    }
-    
-    return bpn;
-}
-
-
-/**
- *  
- **/
 int SinglePageWrite()
 {
 
     /* Function flag, default value equals 0(successful flag). */
     int err = 0;
 
-    // struct nvm_addr addrs_chunk = nvm_addr_dev2gen(bp->dev, pageno);
-    // size_t ws_min = nvm_dev_get_ws_min(bp->dev);
-    // struct nvm_addr addrs[ws_min];
-    // for (size_t aidx = 0; aidx < ws_min; ++aidx) 
-    // {
-	// 	addrs[aidx].val = addrs_chunk.val;
-	// 	addrs[aidx].l.sectr = (pageno%4096)+aidx;
-	// 	/* printf("aidx: %lu addrs[aidx].val : %lu chunk_addrs[cidx].val %lu addrs[aidx].l.sectr %lu \n",aidx,addrs[aidx].val,chunk_addrs[cidx].val,addrs[aidx].l.sectr); */
-	// }
-
-    // char * temp = new char[20];
-    // for (size_t i = 0; i < Entries.size(); i++)
-    // {
-    //     // printf("Value :%ld has been inserted!\n", ML[Cursize]);
-    //     uint64_t *ML = (uint64_t*) temp;
-    //     ML[0] = Entries[i].key, ML[1] = Entries[i].val;
-    //     for(size_t j= i*sizeof(entry_t)*8,k=0;j<i*sizeof(entry_t)*8+sizeof(entry_t)*8;j++,k++)
-    //     {
-    //             bp->bufs->write[i] = temp[i];
-    //     }
-	// }
+    struct nvm_addr addrs_chunk = nvm_addr_dev2gen(bp->dev, sectorpointer);
+    size_t ws_min = nvm_dev_get_ws_min(bp->dev);
+    struct nvm_addr addrs[ws_min];
+    for (size_t aidx = 0; aidx < ws_min; ++aidx) 
+    {
+	    addrs[aidx].val = addrs_chunk.val;
+	 	addrs[aidx].l.sectr =(sectorpointer%4096)+aidx;
+	 	/* printf("aidx: %lu addrs[aidx].val : %lu chunk_addrs[cidx].val %lu addrs[aidx].l.sectr %lu \n",aidx,addrs[aidx].val,chunk_addrs[cidx].val,addrs[aidx].l.sectr); */
+	}
         
-    // // Write value into page. 
-    // err = nvm_cmd_write(bp->dev, addrs, ws_min,bp->bufs->write, NULL,0x0, NULL);
-    // if(err == 0) 
-    // {
-    //     //printf("Insert completion! Insert sectors: %ld\n",sectorpointer);
-    //     PointerRenew(ws_min);   /* update pointers! */
-    // }
-    // else
-    // {
-    //         EMessageOutput("Page writing failed in "+ Uint64toString(pageno)+"\n", 4598);
-    // }
+    /* Write value into page. */ 
+    char * databuffer = (char*) Pagedata;
+    for(int i=0;i<2048*8;i++)
+    {
+        bp->bufs->write[i] = databuffer[i];
+    }
+    err = nvm_cmd_write(bp->dev, addrs, ws_min,bp->bufs->write, NULL,0x0, NULL);
+    if(err == 0) 
+    {
+        //printf("Insert completion! Insert sectors: %ld\n",sectorpointer);
+        PointerRenew(ws_min);   /* update pointers! */
+    }
+    else
+    {
+        EMessageOutput("Page writing failed in "+ Uint64toString(sectorpointer)+"\n", 4598);
+    }
 
     return err;
 
--- src/TNCTree/StoreTier/syncstore.cpp
+++ src/TNCTree/StoreTier/syncstore.cpp
@@ -3,22 +3,22 @@
 #include "../../Backend/SSDRead/reader.h"
 #include "../../Backend/IODisk/WriteDisk.h"
 
+uint32_t offset = 0;
+
 uint32_t SyncWrite(SKey key1, SValue value)
 {
 
     /* Returned offset */
-    uint32_t offset = 0;
-
 
 	if(indexs >= CalculatePageCapacity(sizeof(TNCEntry)))
     {
-
         SinglePageWrite();
         offset += sectorpointer%4096==0 && sectorpointer!=0?0x400000:0x00000000 ;
         offset += 0x1000; 
+        offset = offset & 0xFFFFF000;
         indexs = 0;
-    }
-	 
+    } 
+
 	Pagedata[indexs].key = key1;
 	Pagedata[indexs].val = value;
 	++indexs;

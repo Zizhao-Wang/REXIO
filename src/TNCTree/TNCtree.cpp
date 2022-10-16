69fb4a03ebd026b4dc2656c3fd4e9771df15f626
ZizhaoWa <81192wzz@gmail.com>
2022-10-16 16:59:00 +0000

    sync

--- src/TNCTree/TNCtree.cpp
+++ src/TNCTree/TNCtree.cpp
@@ -89,7 +89,7 @@ void TNCtreePort(void)
      printf("Read count:%d write:%d erase:%d\n",reads,write,erase);
      std::cout << "Total Time of workload B: " <<(double)(endTime - startTime) / CLOCKS_PER_SEC << "s\n\n\n";
 
-     fifocache
+     fifocache.Clear();
 
      /* workload c: read only, 50% in it, 50% not in it */
      startTime = clock();

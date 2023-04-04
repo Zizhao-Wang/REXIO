aa9d4e739ed1914ae031095001cf00654e292401
ZizhaoWa <81192wzz@gmail.com>
2023-04-04 08:33:44 +0000

    sync

---
+++ src/LOCS/locs.h
@@ -0,0 +1,9 @@
+/**
+ * @date    3/4/2022 
+ * @author  zz.wang
+ * @details
+ *          This file implemented the data structure of the node in Hash Table!
+ *
+ * IDENTIFICATION:
+ *          src/LOCS/locs.h
+ */
